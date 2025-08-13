bfa53afd3583ab162397afb716ac382b23ca76f4
maryaalshameri <marynaalshameri@gmail.com>
2025-08-13 21:54:17 +0300

    add config file with TAX_RATE, GLOBAL_DISCOUNT, LOG_PATH, etc.

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'APP_ENV'         => 'local',
+    'CURRENCY'        => 'USD',
+    'TAX_RATE'        => 0.15,   // 15%
+    'GLOBAL_DISCOUNT' => 0.05,   // 5%
+    'LOG_PATH'        => __DIR__ . '/storage/logs/app.log',
+];
